7e72b39ec9835be8b904670026b1d02d12a7fc55
maggieleebonnie <82593792+maggieleebonnie@users.noreply.github.com>
2021-05-17 12:11:25 +0200

    Update HelloServlet.java

--- src/main/java/com/formation/HelloServlet.java
+++ src/main/java/com/formation/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends HttpServlet {
        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Hello from servlet hello!!!");
+		response.getWriter().append("Hello from servlet Hello hello!!!");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
